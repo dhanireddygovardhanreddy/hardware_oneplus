2248975b80679f45b5ba18a2321ec0f2e25387dc
chandu078 <chandudyavanapelli03@gmail.com>
2022-04-06 13:01:21 +0530

    kebab: fingerprint: Misc changes
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- hidl/fingerprint/BiometricsFingerprint.cpp
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -33,7 +33,6 @@
 #define OP_DISPLAY_NOTIFY_PRESS 9
 #define OP_DISPLAY_SET_DIM 10
 
-#define AUTH_STATUS_PATH  "/sys/class/drm/card0-DSI-1/auth_status"
 #define POWER_STATUS_PATH "/sys/class/drm/card0-DSI-1/power_status"
 
 
@@ -88,7 +87,6 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t) {
 }
 
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, float) {
-    this->isCancelled = 0;
     mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1); // Fixme! workaround for in-app fod auth
     mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 1);
 
@@ -213,11 +211,9 @@ Return<uint64_t> BiometricsFingerprint::getAuthenticatorId() {
 }
 
 Return<RequestStatus> BiometricsFingerprint::cancel() {
-    this->isCancelled = 1;
     mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
     mVendorFpService->updateStatus(OP_FINISH_FP_ENROLL);
     mVendorFpService->updateStatus(OP_ENABLE_FP_LONGPRESS);
-    set(AUTH_STATUS_PATH, 2);
 
     return ErrorFilter(mDevice->cancel(mDevice));
 }
@@ -246,13 +242,8 @@ Return<RequestStatus> BiometricsFingerprint::setActiveGroup(uint32_t gid,
 
 Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId,
         uint32_t gid) {
+    mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
     set(POWER_STATUS_PATH, 1);
-    if (this->isCancelled) {
-        mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
-        set(AUTH_STATUS_PATH, 0);
-    } else {
-        set(AUTH_STATUS_PATH, 1);
-    }
     mVendorFpService->updateStatus(OP_ENABLE_FP_LONGPRESS);
 
     return ErrorFilter(mDevice->authenticate(mDevice, operationId, gid));
@@ -336,7 +327,6 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t *msg) {
                 if (!thisPtr->mClientCallback->onError(devId, result, vendorCode).isOk()) {
                     ALOGE("failed to invoke fingerprint onError callback");
                 }
-                getInstance()->onFingerUp();
             }
             break;
         case FINGERPRINT_ACQUIRED: {
